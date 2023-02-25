e5e5b2687281d2cc3571ad2a2c4f812ac07655f8
Pratyush <pratyush.kumar@vecrear.com>
2023-02-25 20:18:39 +0530

    Password component added

--- client/src/components/Password.js
+++ client/src/components/Password.js
@@ -27,7 +27,7 @@ const Password = () => {
           <div className="title flex flex-col items-center">
             <h4 className="text-5xl font-bold">Hello Again!</h4>
             <span className="py-4 text-xl w-2/3 text-center text-gray-500">
-              Explore More by connecting withus.
+              Explore More by connecting with us.
             </span>
           </div>
 
